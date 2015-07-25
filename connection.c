projectara/greybus

commit 82edfac17e5fe42623fbad9b88b9264fc89bd846

connection: don't send connected/disconnected events for SVC connection
SVC is also a special protocol (like control) and AP doesn't need to
send (dis)connected events for its cport.

Lets skip them.

Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

connection.c
@@ -289,7 +289,7 @@ int gb_connection_init(struct gb_connection *connection)
 	 * Inform Interface about Active CPorts. We don't need to do this
 	 * operation for control cport.
 	 */
-	if (cport_id != GB_CONTROL_CPORT_ID) {
+	if (cport_id != GB_CONTROL_CPORT_ID && cport_id != GB_SVC_CPORT_ID) {
 		struct gb_control *control = connection->bundle->intf->control;
 
 		ret = gb_control_connected_operation(control, cport_id);
@@ -341,7 +341,7 @@ void gb_connection_exit(struct gb_connection *connection)
 	 * Inform Interface about In-active CPorts. We don't need to do this
 	 * operation for control cport.
 	 */
-	if (cport_id != GB_CONTROL_CPORT_ID) {
+	if (cport_id != GB_CONTROL_CPORT_ID && cport_id != GB_SVC_CPORT_ID) {
 		struct gb_control *control = connection->bundle->intf->control;
 		int ret;
 

MotorolaMobilityLLC/motorola-external-greybus

commit 351d46e556b25d0cfdbb075b78cd89ae5fc14f01

connection: compare hd_cport_id for matching SVC protocol
SVC protocol isn't per interface, but per entire entire endo. And so svc
cport id must be matched against hd_cport_id, not per-interface cport
id.

Fixes: 82edfac17e5f ("connection: don't send connected/disconnected events for SVC connection")
Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

connection.c
@@ -344,7 +344,8 @@ int gb_connection_init(struct gb_connection *connection)
 	 * Inform Interface about Active CPorts. We don't need to do this
 	 * operation for control cport.
 	 */
-	if (cport_id != GB_CONTROL_CPORT_ID && cport_id != GB_SVC_CPORT_ID) {
+	if (cport_id != GB_CONTROL_CPORT_ID &&
+	    connection->hd_cport_id != GB_SVC_CPORT_ID) {
 		struct gb_control *control = connection->bundle->intf->control;
 
 		ret = gb_control_connected_operation(control, cport_id);
@@ -396,7 +397,8 @@ void gb_connection_exit(struct gb_connection *connection)
 	 * Inform Interface about In-active CPorts. We don't need to do this
 	 * operation for control cport.
 	 */
-	if (cport_id != GB_CONTROL_CPORT_ID && cport_id != GB_SVC_CPORT_ID) {
+	if (cport_id != GB_CONTROL_CPORT_ID &&
+	    connection->hd_cport_id != GB_SVC_CPORT_ID) {
 		struct gb_control *control = connection->bundle->intf->control;
 		int ret;
 
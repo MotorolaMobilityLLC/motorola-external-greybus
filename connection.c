projectara/greybus

commit afde17fe0b61ccb07106364b38c4bec6d577198a

greybus/connection: fix jump label on device_add failure
On device_add() failure in gb_connection_create_range() we jump to
err_remove_ida. Instead we should be jumping to err_free_connection, so
change the flow to accomodate.

Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

connection.c
@@ -238,7 +238,7 @@ gb_connection_create_range(struct greybus_host_device *hd,
 		pr_err("failed to add connection device for cport 0x%04hx\n",
 			cport_id);
 
-		goto err_remove_ida;
+		goto err_free_connection;
 	}
 
 	spin_lock_irq(&gb_connections_lock);

MotorolaMobilityLLC/motorola-external-greybus

commit 48d10a50a9210a0cbe0bf5ecc0327a2a789149f8

connection: Don't free resources freed by gb_connection_release()
We are already doing put_device() here and so don't need to free
resources directly, except ida.

Fixes: afde17fe0b61 ("greybus/connection: fix jump label on device_add failure")
Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

connection.c
@@ -270,7 +270,7 @@ gb_connection_create_range(struct greybus_host_device *hd,
 		pr_err("failed to add connection device for cport 0x%04hx\n",
 			cport_id);
 
-		goto err_free_kfifo;
+		goto err_remove_ida;
 	}
 
 	spin_lock_irq(&gb_connections_lock);
@@ -296,8 +296,6 @@ gb_connection_create_range(struct greybus_host_device *hd,
 
 	return connection;
 
-err_free_kfifo:
-	kfifo_free(&connection->ts_kfifo);
 err_destroy_wq:
 	destroy_workqueue(connection->wq);
 err_free_connection:
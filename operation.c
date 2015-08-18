projectara/greybus

commit cb0ef0c019ab586a8af234d150f506b1bc92164b

operation: print message type on errors
This can be very useful debug information, print it.

Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

operation.c
@@ -235,8 +235,8 @@ static void gb_operation_request_handle(struct gb_operation *operation)
 	ret = gb_operation_response_send(operation, status);
 	if (ret) {
 		dev_err(&operation->connection->dev,
-			"failed to send response %d: %d\n",
-			status, ret);
+			"failed to send response %d for type 0x%02hhx: %d\n",
+			status, operation->type, ret);
 			return;
 	}
 }
@@ -794,7 +794,8 @@ void greybus_message_sent(struct greybus_host_device *hd,
 	if (message == operation->response) {
 		if (status) {
 			dev_err(&connection->dev,
-				"error sending response: %d\n", status);
+				"error sending response type 0x%02hhx: %d\n",
+				operation->type, status);
 		}
 		gb_operation_put_active(operation);
 		gb_operation_put(operation);
@@ -867,8 +868,8 @@ static void gb_connection_recv_response(struct gb_connection *connection,
 	message = operation->response;
 	message_size = sizeof(*message->header) + message->payload_size;
 	if (!errno && size != message_size) {
-		dev_err(&connection->dev, "bad message size (%zu != %zu)\n",
-			size, message_size);
+		dev_err(&connection->dev, "bad message (0x%02hhx) size (%zu != %zu)\n",
+			size, message_size, message->header->type);
 		errno = -EMSGSIZE;
 	}
 
@@ -913,8 +914,9 @@ void gb_connection_recv(struct gb_connection *connection,
 	msg_size = le16_to_cpu(header.size);
 	if (size < msg_size) {
 		dev_err(&connection->dev,
-			"incomplete message received: 0x%04x (%zu < %zu)\n",
-			le16_to_cpu(header.operation_id), size, msg_size);
+			"incomplete message received for type 0x%02hhx: 0x%04x (%zu < %zu)\n",
+			header.type, le16_to_cpu(header.operation_id), size,
+			msg_size);
 		return;		/* XXX Should still complete operation */
 	}
 
@@ -1019,8 +1021,8 @@ int gb_operation_sync_timeout(struct gb_connection *connection, int type,
 
 	ret = gb_operation_request_send_sync_timeout(operation, timeout);
 	if (ret) {
-		dev_err(&connection->dev, "synchronous operation failed: %d\n",
-			ret);
+		dev_err(&connection->dev, "synchronous operation failed: 0x%02hhx (%d)\n",
+			type, ret);
 	} else {
 		if (response_size) {
 			memcpy(response, operation->response->payload,

MotorolaMobilityLLC/motorola-external-greybus

commit 4de68d8d1acdbe31ecc413317a5df2cc9696d44a

operation: Fix wrong order of arguments
The order of arguments is wrong and that shows up as a warning only on
64 bit machines.

Fixes: cb0ef0c019ab ("operation: print message type on errors")
Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

operation.c
@@ -869,7 +869,7 @@ static void gb_connection_recv_response(struct gb_connection *connection,
 	message_size = sizeof(*message->header) + message->payload_size;
 	if (!errno && size != message_size) {
 		dev_err(&connection->dev, "bad message (0x%02hhx) size (%zu != %zu)\n",
-			size, message_size, message->header->type);
+			message->header->type, size, message_size);
 		errno = -EMSGSIZE;
 	}
 
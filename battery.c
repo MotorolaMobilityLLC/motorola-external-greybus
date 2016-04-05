2d8e0a8bfa1ef71f095dcc814254709178a1b4f7
Jason Hrycay <jason.hrycay@motorola.com>
2016-04-05 09:58:34 -0500

    fixup! battery: Override power supply dev release pointer
    Change-Id: I1c74f4c94acfdc20d4fee3ae70ddd8e48ae6470c
    Reviewed-on: http://gerrit.mot.com/841251
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Dmitriy Gruzman <dgruzman@motorola.com>
    Reviewed-by: Gregory Meiste <w30289@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    SME-Granted: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jason Hrycay <jason.hrycay@motorola.com>
    Tested-by: Jason Hrycay <jason.hrycay@motorola.com>

--- battery.c
+++ battery.c
@@ -226,7 +226,7 @@ static int get_property(struct power_supply *b,
 	mutex_lock(&gb->conn_lock);
 	if (!gb->connection) {
 		mutex_unlock(&gb->conn_lock);
-		pr_err("%s: upply already free'd: %s\n", __func__, b->name);
+		pr_err("%s: supply already free'd: %s\n", __func__, b->name);
 		return -ENODEV;
 	}
 
